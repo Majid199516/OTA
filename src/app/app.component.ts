09f563cdc3b9ef7ad0742eb9214993c665a04bb2
majid199516 <majidsharafi1995@gmail.com>
2025-07-27 16:36:14 +0330

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HomePageComponent } from "./core/home-page/home-page.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [ HomePageComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'ota';
+}
